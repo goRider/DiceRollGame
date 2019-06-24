03f6eabd1a12c8cd8e3eac37423b57c6b175070c
goRider <devgwo18@gmail.com>
2019-06-24 17:47:05 -0500

    Add 2 random variable to dice rolls

--- CrapsGame/Form1.cs
+++ CrapsGame/Form1.cs
@@ -21,8 +21,62 @@ namespace CrapsGame
         {
             Random r = new Random();
             int iRnd = new int();
+            int iRnd2 = new int();
 
             iRnd = r.Next(0, 6);
+            iRnd2 = r.Next(0, 6);
+
+            if (iRnd == 0)
+            {
+                pbDiceShow1.Image = pbDice1.Image;
+            }
+            else if(iRnd == 1)
+            {
+                pbDiceShow1.Image = pbDice2.Image;
+            }
+            else if (iRnd == 2)
+            {
+                pbDiceShow1.Image = pbDice3.Image;
+            }
+            else if (iRnd == 3)
+            {
+                pbDiceShow1.Image = pbDice4.Image;
+            }
+            else if (iRnd == 4)
+            {
+                pbDiceShow1.Image = pbDice5.Image;
+            }
+            else
+            {
+                pbDiceShow1.Image = pbDice6.Image;
+            }
+
+            // Second dice roll image
+
+            if (iRnd2 == 0)
+            {
+                pbDiceShow2.Image = pbDice1.Image;
+            }
+            else if (iRnd2 == 1)
+            {
+                pbDiceShow2.Image = pbDice2.Image;
+            }
+            else if (iRnd2 == 2)
+            {
+                pbDiceShow2.Image = pbDice3.Image;
+            }
+            else if (iRnd2 == 3)
+            {
+                pbDiceShow2.Image = pbDice4.Image;
+            }
+            else if (iRnd2 == 4)
+            {
+                pbDiceShow2.Image = pbDice5.Image;
+            }
+            else
+            {
+                pbDiceShow2.Image = pbDice6.Image;
+            }
         }
     }
 }
